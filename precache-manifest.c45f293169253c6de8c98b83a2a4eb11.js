9c34ed1a1593318496cb61bc7c1ba32a5d671b21
Dhiraj <dhirajbhakta110@gmail.com>
2020-03-21 00:49:34 +0530

    for release

---
+++ precache-manifest.c45f293169253c6de8c98b83a2a4eb11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1a89808afd6bc8d45d6f9313b7cfff9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53842d7b61f65ea561f5",
+    "url": "/static/css/2.0cbfcbd1.chunk.css"
+  },
+  {
+    "revision": "91a5487317ef97d85072",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "53842d7b61f65ea561f5",
+    "url": "/static/js/2.c72f992b.chunk.js"
+  },
+  {
+    "revision": "21bef15af46e705355ddcaa1070e8aac",
+    "url": "/static/js/2.c72f992b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91a5487317ef97d85072",
+    "url": "/static/js/main.0aada9d9.chunk.js"
+  },
+  {
+    "revision": "c53db67445f24fd01f47",
+    "url": "/static/js/runtime-main.3bfb7ef8.js"
+  }
+]);
